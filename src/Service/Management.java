d82d283cb088f28ae71c1055a6af489c08e22c3c
devJWL <jwlee4450@gmail.com>
2024-01-13 01:47:56 +0900

    학생 등록 필수과목 등록까지 구현

--- src/Service/Management.java
+++ src/Service/Management.java
@@ -1,6 +1,10 @@
 package Service;
 import Data.DataBase;
+import Resources.Subject;
 import Util.Options.*;
+import Util.Subject.MandatorySubject;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import static Util.Options.MenuOption.*;
 import static Util.Options.MainMenuOption.*;
@@ -10,6 +14,8 @@ import static Util.Options.ScoreRegisterMenuOption.*;
 import static Util.Options.StudentInquireMenuOption.*;
 import static Util.Options.StudentMenuOption.*;
 import static Util.Options.StudentRegisterMenuOption.*;
+import static Util.Options.YesOrNoOption.*;
+import static Util.Subject.MandatorySubject.*;
 
 
 // App
@@ -54,6 +60,7 @@ public class Management{
             System.out.println(printMenuOption.getStringData(STUDENT_MAIN_MENU));
             StudentMenuOption select = dataBase.getStudentMenuOptionMap()
                     .get(returnVaildOutput(STUDENT_MENU_OPTION_REGISTER.ordinal(), STUDENT_MENU_OPTION_BACK.ordinal()));
+
             switch (select) {
                 case STUDENT_MENU_OPTION_REGISTER-> {
                     registerStudent();
@@ -83,9 +90,10 @@ public class Management{
             System.out.println(printMenuOption.getStringData(STUDENT_REGISTER_MENU));
             StudentRegisterMenuOption select = dataBase.getStudentRegisterMenuOptionMap()
                     .get(returnVaildOutput(STUDENT_REGISTER_MENU_OPTION_ERROR.ordinal(), STUDENT_REGISTER_MENU_OPTION_BACK.ordinal()));
+
             switch (select) {
                 case STUDENT_REGISTER_MENU_OPTION_REGISTER-> {
-
+                    registerStudentHelper();
                 }
                 case STUDENT_REGISTER_MENU_OPTION_BACK-> {
                     return;
@@ -98,11 +106,75 @@ public class Management{
         }
     }
 
+    private void registerStudentHelper() {
+        System.out.println("수강생의 이름을 입력해주세요");
+        String studentName = sc.nextLine();
+        System.out.println("수강생의  상태를 입력해주세요");
+        String status = sc.nextLine();
+        List<Subject> subjectList = new ArrayList<>();
+        addMandatorySubjectHelper(studentName, subjectList);
+    }
+
+    private void addMandatorySubjectHelper(String studentName, List<Subject> subjectList) {
+        System.out.println("수강생이 수강 중인 필수과목 목록을 입력해주세요");
+        System.out.println("필수과목 목록");
+        int no = 1;
+        for (MandatorySubject mandatorySubject : MandatorySubject.values()) {
+            System.out.printf("%3d. %10s\n", no, mandatorySubject.name());
+            ++no;
+        }
+        System.out.println();
+
+        boolean isEnd = false;
+        do {
+            System.out.println("과목에 해당하는 숫자를 입력해주세요");
+            MandatorySubject mandatorySubject = dataBase.getMandatorySubjectMap()
+                    .get(returnVaildOutput(JAVA.ordinal(), MYSQL.ordinal()));
+
+            if (mandatorySubject == MANDATORY_SUBJECT_ERROR) {
+                System.out.println("잘못된 입력 혹은 범위 밖의 숫자를 입력하셨습니다.");
+            }
+            // 올바른 입력
+            else {
+                String key = studentName + mandatorySubject.name();
+                if (!dataBase.getSubjectSet().contains(key)) {
+                    dataBase.getSubjectSet().add(key);
+                    subjectList.add(new Subject(mandatorySubject.name(), true));
+                }
+                else {
+                    System.out.printf("%s 과목은 이미 추가 되었습니다. 다른 과목을 선택해주세요.\n", mandatorySubject.name());
+                }
+            }
+            System.out.println("과목을 계속 선택하시겠습니까?");
+            YesOrNoOption yesOrNoOption = YesNoInputHelper();
+            if (yesOrNoOption == YES_OR_NO_OPTION_YES) {
+                continue;
+            }
+            if (subjectList.size() >= 3){
+                isEnd = true;
+            };
+        }while(isEnd);
+    }
+
+    private YesOrNoOption YesNoInputHelper() {
+        YesOrNoOption yesOrNoOption;
+        do {
+            System.out.println("1. 네    2. 아니요");
+            yesOrNoOption = dataBase.getYesOrNoOptionMap()
+                    .get(returnVaildOutput(YES_OR_NO_OPTION_YES.ordinal(), YES_OR_NO_OPTION_NO.ordinal()));
+            if (yesOrNoOption == YES_OR_NO_OPTION_ERROR) {
+                System.out.println("잘못된 입력이거나 범위 밖에 값을 입력하셨습니다.");
+            }
+        }while(yesOrNoOption != YES_OR_NO_OPTION_ERROR);
+        return yesOrNoOption;
+    }
+
     private void inquireStudent() {
         while(true) {
             System.out.println(printMenuOption.getStringData(STUDENT_INQUIRE_MENU));
             StudentInquireMenuOption select = dataBase.getStudentInquireMenuOptionMap()
                     .get(returnVaildOutput(STUDENT_INQUIRE_MENU_OPTION_ID.ordinal(), STUDENT_INQUIRE_MENU_OPTION_BACK.ordinal()));
+
             switch (select) {
                 case STUDENT_INQUIRE_MENU_OPTION_ID-> {
                     // 1--> id로 조회
@@ -138,6 +210,7 @@ public class Management{
             System.out.println(printMenuOption.getStringData(SCORE_MAIN_MENU));
             ScoreMenuOption select = dataBase.getScoreMenuOptionMap()
                     .get(returnVaildOutput(SCORE_MENU_OPTION_REGISTER.ordinal(), SCORE_MENU_OPTION_BACK.ordinal()));
+
             switch (select) {
                 case SCORE_MENU_OPTION_REGISTER -> {
                     registerScore();
@@ -165,6 +238,7 @@ public class Management{
             System.out.println(printMenuOption.getStringData(SCORE_INQUIRE_MENU));
             ScoreRegisterMenuOption select = dataBase.getScoreRegisterMenuOptionMap()
                     .get(returnVaildOutput(SCORE_REGISTER_MENU_OPTION_REGISTER.ordinal(), SCORE_REGISTER_MENU_OPTION_BACK.ordinal()));
+
             switch (select) {
                 case SCORE_REGISTER_MENU_OPTION_REGISTER-> {
                     // 점수 등록하기
@@ -185,6 +259,7 @@ public class Management{
             System.out.println(printMenuOption.getStringData(SCORE_INQUIRE_MENU));
             StudentInquireMenuOption select = dataBase.getStudentInquireMenuOptionMap()
                     .get(returnVaildOutput(STUDENT_INQUIRE_MENU_OPTION_ID.ordinal(), STUDENT_INQUIRE_MENU_OPTION_BACK.ordinal()));
+
             switch (select) {
                 case STUDENT_INQUIRE_MENU_OPTION_ID-> {
                     // 1. id로 검색
@@ -206,6 +281,7 @@ public class Management{
         System.out.println(printMenuOption.getStringData(SCORE_CHANGE_MENU));
         ScoreChangeMenuOption select = dataBase.getScoreChangeMenuOptionMap()
                 .get(returnVaildOutput(SCORE_CHANGE_MENU_OPTION_CHANGE.ordinal(), SCORE_CHANGE_MENU_OPTION_BACK.ordinal()));
+
         switch (select) {
             case SCORE_CHANGE_MENU_OPTION_CHANGE-> {
                 // 1. 변경하기
