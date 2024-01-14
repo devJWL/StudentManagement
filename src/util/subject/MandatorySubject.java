14d71f34dcbe022c2f0bcdfdc2b34004f9f32e67
devJWL <jwlee4450@gmail.com>
2024-01-14 15:17:54 +0900

    Enum 클래스 속성에 대한 리스트 반환 메소드 추가

--- src/util/subject/MandatorySubject.java
+++ src/util/subject/MandatorySubject.java
@@ -1,5 +1,10 @@
 package util.subject;
 
+import util.options.StudentStatus;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public enum MandatorySubject {
     MANDATORY_SUBJECT_ERROR("Error"),
     JAVA("Java"),
@@ -10,14 +15,23 @@ public enum MandatorySubject {
 
     private final String subjectName;
     private static final MandatorySubject[] mandatorySubjects = MandatorySubject.values();
-
+    private static final List<String> mandatorySubjectStringList = new ArrayList<>();
+    static {
+        for (MandatorySubject mandatorySubject: mandatorySubjects) {
+            mandatorySubjectStringList.add(mandatorySubject.getSubjectName());
+        }
+    }
     MandatorySubject(String subjectName) {
         this.subjectName = subjectName;
     }
+
     public String getSubjectName() {
         return subjectName;
     }
     public static MandatorySubject get(int index) {
         return mandatorySubjects[index];
+    }
+    public static List<String> getMandatorySubjectStringList() {
+        return mandatorySubjectStringList;
     }
 }
--- src/util/subject/SelectSubject.java
+++ src/util/subject/SelectSubject.java
@@ -1,5 +1,8 @@
 package util.subject;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public enum SelectSubject {
     SELECT_SUBJECT_ERROR("ERROR"),
     DESIGN_PATTERN("Design Pattern"),
@@ -9,14 +12,24 @@ public enum SelectSubject {
 
     private final String subjectName;
     private static final SelectSubject[] selectSubjects = SelectSubject.values();
-
+    private static final List<String> selectSubjectStringList = new ArrayList<>();
+    static {
+        for (SelectSubject selectSubject : selectSubjects) {
+            selectSubjectStringList.add(selectSubject.getSubjectName());
+        }
+    }
     SelectSubject(String subjectName) {
         this.subjectName = subjectName;
     }
+
+
     public String getSubjectName() {
         return subjectName;
     }
     public static SelectSubject get(int index) {
         return selectSubjects[index];
+    }
+    public static List<String> getSelectSubjectStringList() {
+        return selectSubjectStringList;
     }
 }
