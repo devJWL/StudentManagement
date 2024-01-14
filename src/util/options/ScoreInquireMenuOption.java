e874927d34b93565a3d02841e674e1f63b8772d4
devJWL <jwlee4450@gmail.com>
2024-01-14 18:01:05 +0900

    점수 조회 방법에 대한 Enum클래스 추가

--- src/util/options/ScoreInquireMenuOption.java
+++ src/util/options/ScoreInquireMenuOption.java
@@ -1,4 +1,13 @@
 package util.options;
 
 public enum ScoreInquireMenuOption {
+    SCORE_INQUIRE_MENU_OPTION_ERROR,
+    SCORE_INQUIRE_MENU_OPTION_ID,
+    SCORE_INQUIRE_MENU_OPTION_STATUS;
+
+
+    private static final ScoreInquireMenuOption[] scoreChangeMenuOptions = ScoreInquireMenuOption.values();
+    public static ScoreInquireMenuOption get(int index) {
+        return scoreChangeMenuOptions[index];
+    }
 }
