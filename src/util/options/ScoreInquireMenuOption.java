56ba844ba29187199bf88fed1da493522a6603d8
devJWL <jwlee4450@gmail.com>
2024-01-14 18:35:57 +0900

    변경사항 없음

--- src/util/options/ScoreInquireMenuOption.java
+++ src/util/options/ScoreInquireMenuOption.java
@@ -3,8 +3,8 @@ package util.options;
 public enum ScoreInquireMenuOption {
     SCORE_INQUIRE_MENU_OPTION_ERROR,
     SCORE_INQUIRE_MENU_OPTION_ID,
-    SCORE_INQUIRE_MENU_OPTION_STATUS;
-
+    SCORE_INQUIRE_MENU_OPTION_STATUS,
+    SCORE_INQUIRE_MENU_OPTION_BACK;
 
     private static final ScoreInquireMenuOption[] scoreChangeMenuOptions = ScoreInquireMenuOption.values();
     public static ScoreInquireMenuOption get(int index) {
