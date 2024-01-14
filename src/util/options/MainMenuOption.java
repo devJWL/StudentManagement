b063151aa4cc0d0a35e0dafba3577375a4ce94bc
devJWL <jwlee4450@gmail.com>
2024-01-14 15:04:03 +0900

    변경사항 없음

--- src/util/options/MainMenuOption.java
+++ src/util/options/MainMenuOption.java
@@ -4,5 +4,10 @@ public enum MainMenuOption {
     MAIN_MENU_OPTION_ERROR,
     MAIN_MENU_OPTION_STU,
     MAIN_MENU_OPTION_SCORE,
-    MAIN_MENU_OPTION_EXIT
+    MAIN_MENU_OPTION_EXIT;
+
+    private static final MainMenuOption[] mainMenuOptions = MainMenuOption.values();
+    public static MainMenuOption get(int index) {
+        return mainMenuOptions[index];
+    }
 }
