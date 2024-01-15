408e394ade570c688bc420dd2f044efdb66e342e
devJWL <jwlee4450@gmail.com>
2024-01-15 13:55:26 +0900

    return ValidOutput 문자열이 00으로 시작하는 경우 유효성체크 추가, end를 모든 선택에 대해 뒤로가기 구현을 위해 "-1"을 입력 받은 경우 end를 반환하도록 구현

--- src/util/Util.java
+++ src/util/Util.java
@@ -10,6 +10,7 @@ public class Util {
     private final Scanner sc;
     private final PrintMenuOption printMenuOption;
     private final Map<String, Boolean> OptionOrMandatoryMap = new HashMap<>();
+    private final int RETURN_NUMBER_INIT = -1;
 
     public Util(Scanner sc, PrintMenuOption printMenuOption) {
         this.sc = sc;
@@ -23,23 +24,26 @@ public class Util {
 
     // =================================== 선택 입력 예외 처리 =================================================
     public int returnValidOutput(int start, int end) {
-        int returnNumber = -1;
+        int returnNumber = RETURN_NUMBER_INIT;
         String input;
         do {
             System.out.print("> ");
             input = sc.nextLine();
-            // 문자열의 길이가 0이 아니고, 숫자만으로 구성되어있는지 확인
-            if (input.length() != 0 && isDigit(input)) {
+            if (input.equals("-1")) {
+                return end;
+            }
+            // 문자열의 길이가 0이 아니고, "0000"과 같은 문자열이 아니고, 숫자로만 구성
+            if (input.length() > 0 && !input.startsWith("00") && isDigit(input)) {
                 int number = Integer.parseInt(input);
                 // 현재 메뉴옵션의 범위에 존재하는 숫자인지 확인
-                if (number >= start && number <= end) {
+                if (number > start && number < end) {
                     returnNumber = number;
                 }
             }
-            if (returnNumber == -1) {
+            if (returnNumber == -2) {
                 System.out.println(printMenuOption.getStringData(INPUT_ERROR_MENU));
             }
-        } while (returnNumber == -1);
+        } while (returnNumber == -2);
         return returnNumber;
     }
 
